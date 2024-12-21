671a12a71a42daba68a09f92f17be445ab51e49a
Felipe Guajardo <felipgg@gmail.com>
2024-12-21 15:35:51 -0300

    Barebones consumer of websub hub

--- cmd/conceal-bot-go/main.go
+++ cmd/conceal-bot-go/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"context"
+	"io"
 	"log"
+	"net/http"
+	"net/url"
 	"os"
 	"time"
 	_ "time/tzdata"
@@ -16,10 +19,66 @@ import (
 	"github.com/pipexlul/conceal-bot-go/internal/pkg/env"
 )
 
-const funnyStatus = "Epic Anya very voice!" 
+const (
+	funnyStatus = "Are we some kind of tenet?"
+
+	hubURL      = "http://localhost:50000/hub"
+	hubTopic    = "earthquakes"
+	callbackURL = "http://localhost:50001/earthquakes"
+)
+
+func subscribeToRecentEarthquakes() {
+	query := url.Values{}
+	query.Set("hub.mode", "subscribe")
+	query.Set("hub.topic", hubTopic)
+	query.Set("hub.callback", callbackURL)
+
+	url := hubURL + "?" + query.Encode()
+
+	req, err := http.NewRequest(http.MethodPost, url, nil)
+	if err != nil {
+		log.Fatalf("Could not create request to subscribe to earthquakes: %v", err)
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatalf("Could not subscribe to earthquakes: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
+		log.Fatalf("Wrong status code when subscribing to earthquakes: %v", resp.StatusCode)
+	}
+
+	log.Print("Successfully subscribed to earthquakes\n")
+}
+
+func websubCallbackHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		// TODO: Add challenge verifications
+		return
+	}
+
+	if r.Method == http.MethodPost {
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "failed to ready post request body", http.StatusInternalServerError)
+			return
+		}
+		defer r.Body.Close()
+
+		log.Printf("Received message from WebSub hub: \n%s", string(body))
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+
+	http.Error(w, "Invalid method: "+r.Method, http.StatusMethodNotAllowed)
+}
 
 var mongoClient *mongo.Client
 
+// Unused until we need mongo
 func connectMongo() {
 	mongoURI := os.Getenv("MONGO_URI")
 	if mongoURI == "" {
@@ -42,6 +101,7 @@ func connectMongo() {
 	mongoClient = client
 }
 
+// Unused until we need mongo
 func disconnectMongo() {
 	if mongoClient != nil {
 		if err := mongoClient.Disconnect(context.Background()); err != nil {
@@ -78,7 +138,7 @@ func main() {
 		log.Println("Discord session closed")
 	}()
 
-	registererCtx, cancelFunc := context.WithTimeout(context.Background(), 30*time.Second)
+	registererCtx, cancelFunc := context.WithTimeout(context.Background(), time.Second*30)
 	defer cancelFunc()
 
 	// TODO: Use a more centralized command registerer
@@ -90,6 +150,21 @@ func main() {
 
 	log.Print("All commands registered!")
 
+	http.HandleFunc("/earthquakes", websubCallbackHandler)
+	httpPort := os.Getenv("PORT")
+	if httpPort == "" {
+		httpPort = "50001"
+	}
+	log.Printf("Listening on port %s", httpPort)
+	httpPort = ":" + httpPort
+	go func() {
+		if err := http.ListenAndServe(httpPort, nil); err != nil {
+			log.Fatalf("Failed to start HTTP server: %v", err)
+		}
+	}()
+
+	subscribeToRecentEarthquakes()
+
 	log.Println("Bot is now running. Press CTRL-C to exit.")
 	select {}
 }
@@ -98,5 +173,9 @@ func onReady(s *discordgo.Session, event *discordgo.Ready) {
 	if err := s.UpdateGameStatus(0, funnyStatus); err != nil {
 		log.Printf("Error updating game status at ready: %v", err)
 	}
-	log.Printf("Ready! Logged in as: %v#%v", s.State.User.Username, s.State.User.Discriminator)
+	log.Printf("Ready! Logged in as: %v#%v with status: %v",
+		s.State.User.Username,
+		s.State.User.Discriminator,
+		funnyStatus,
+	)
 }
